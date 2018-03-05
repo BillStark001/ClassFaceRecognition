8bc0cbdf7476298363f2762c3ceedd783281ea15
BillStark001 <BillStark001@users.noreply.github.com>
2018-03-05 21:44:45 +0800

    vgg recognition(not finished)

--- recognition/main.py
+++ recognition/main.py
@@ -7,34 +7,28 @@ Created on Mon Mar  5 20:29:27 2018
 import keras
 import models
 
+from keras.preprocessing import image
 from imagenet_utils import decode_predictions, preprocess_input
-
 
 import numpy as np
 import cv2
 
-def preprocess(img):
-    
-    return img
-
-input_dimension=(1,64,64,3)
+#input_dimension=(1,64,64,3)
 #input_tensor=np.zeros(input_dimension,dtype='int32')
 #input_tensor=preprocess(input_tensor)
 #input_tensor=K.variable(input_tensor)
-input_tensor=K.placeholder((input_dimension))
-model=models.VGG16(input_tensor=input_tensor)
 
-#Simpler Version
-
+model=models.VGG16()
 
 if __name__=='__main__':
     print(model)
     
-    img_path='../face_0.jpg'
+    img_path='../face_0.png'
     img=image.load_img(img_path,target_size=(224, 224))
     img=image.img_to_array(img)
     img=np.expand_dims(img,axis=0)
     img=preprocess_input(img)
     
+    print('Predicting......')
     preds=model.predict(img)
     print('Predicted:', decode_predictions(preds))
--- recognition/models.py
+++ recognition/models.py
@@ -19,7 +19,6 @@ def VGG16(input_tensor=None):
     # Returns
         A Keras model instance.
     '''
-    include_top=False
     input_shape = (None, None, 3)
     
     if input_tensor is None:
@@ -57,13 +56,6 @@ def VGG16(input_tensor=None):
     x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv3')(x)
     x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
 
-    if include_top:
-        # Classification block
-        x = Flatten(name='flatten')(x)
-        x = Dense(4096, activation='relu', name='fc1')(x)
-        x = Dense(4096, activation='relu', name='fc2')(x)
-        x = Dense(1000, activation='softmax', name='predictions')(x)
-
     # Create model
     model = Model(img_input, x)
 
@@ -72,6 +64,69 @@ def VGG16(input_tensor=None):
                             'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
                             cache_subdir='models')'''
     weights_path='models/vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5'
+    model.load_weights(weights_path)
+    
+    return model
+    
+def VGG16_TOP(input_tensor=None):
+    '''
+    # Arguments
+        input_tensor: optional Keras tensor (i.e. output of `layers.Input()`)
+            to use as image input for the model.
+    # Returns
+        A Keras model instance.
+    '''
+    input_shape = (224, 224, 3)
+    
+    if input_tensor is None:
+        img_input = Input(shape=input_shape)
+    else:
+        if not K.is_keras_tensor(input_tensor):
+            img_input = Input(tensor=input_tensor)
+        else:
+            img_input = input_tensor
+    # Block 1
+    x = Convolution2D(64, 3, 3, activation='relu', border_mode='same', name='block1_conv1')(img_input)
+    x = Convolution2D(64, 3, 3, activation='relu', border_mode='same', name='block1_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+
+    # Block 2
+    x = Convolution2D(128, 3, 3, activation='relu', border_mode='same', name='block2_conv1')(x)
+    x = Convolution2D(128, 3, 3, activation='relu', border_mode='same', name='block2_conv2')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+
+    # Block 3
+    x = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv1')(x)
+    x = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv2')(x)
+    x = Convolution2D(256, 3, 3, activation='relu', border_mode='same', name='block3_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+
+    # Block 4
+    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv1')(x)
+    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv2')(x)
+    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block4_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+
+    # Block 5
+    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv1')(x)
+    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv2')(x)
+    x = Convolution2D(512, 3, 3, activation='relu', border_mode='same', name='block5_conv3')(x)
+    x = MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+
+    # Classification block
+    x = Flatten(name='flatten')(x)
+    x = Dense(4096, activation='relu', name='fc1')(x)
+    x = Dense(4096, activation='relu', name='fc2')(x)
+    x = Dense(1000, activation='softmax', name='predictions')(x)
+
+    # Create model
+    model = Model(img_input, x)
+
+    # load weights
+    '''weights_path = get_file('vgg16_weights_tf_dim_ordering_tf_kernels.h5',
+                            'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/vgg16_weights_tf_dim_ordering_tf_kernels.h5',
+                            cache_subdir='models')'''
+    weights_path='models/vgg16_weights_tf_dim_ordering_tf_kernels.h5'
     model.load_weights(weights_path)
     
     return model
