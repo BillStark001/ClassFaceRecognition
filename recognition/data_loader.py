61066594527f6fcde26a291bc8ceb10d9e1848da
BillStark001 <BillStark001@users.noreply.github.com>
2018-04-07 14:45:48 +0800

    tried to train with aligned faces

--- recognition/data_loader.py
+++ recognition/data_loader.py
@@ -20,6 +20,10 @@ train_dir=root+'\\VAPRBGD\\train\\'
 val_dir=root+'\\VAPRBGD\\val\\'
 train_dir_vgg=root+'\\VGGFACE\\train\\'
 val_dir_vgg=root+'\\VGGFACE\\val\\'
+train_dir_vgg2=root+'\\VGGFACE2\\train\\'
+val_dir_vgg2=root+'\\VGGFACE2\\val\\'
+
+
 
 def create_single_VAPRGBD(file_path,region=(90,112,170,112),thumbnail=(432,324)):
     img=Image.open(file_path)
@@ -30,27 +34,29 @@ def create_single_VAPRGBD(file_path,region=(90,112,170,112),thumbnail=(432,324))
     return mat_small
     
 def create_single_VGGFACE(file_path,resize=(128,128),minsize=64):
-    '''
-    img=Image.open(file_path)
-    img=img.resize(resize)#.convert()
-    img=np.array(img)
-    return img
-    '''
     img = cv2.imread(file_path)
     h, w, c = img.shape
     if c == 1:
-        img = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+        img = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
     elif c != 3:
-        raise ValueError('channels should be 3 or 1!')
-    img = cv2.resize(img, resize)
+        raise ValueError('Channels should be 3 or 1, however %d recieved!'%(c))
+    else:
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    #img = cv2.resize(img, resize)
     return img
 
+#Contrastive Loss
+def get_dir(file_path,mode,ext):
+    if mode=='positive':
+        path=np.random.choice(file_path)
+        path=np.random.choice(glob.glob(path + "/*."+ext),2)
+    elif mode=='negative':
+        path=np.random.choice(file_path,2)
+        path=[np.random.choice(glob.glob(path[0] + "\\*."+ext)),np.random.choice(glob.glob(path[1] + "\\*."+ext))]
+    return path
 
-def create_positive_rgb(file_path,single,ext='jpg'):
-    path=np.random.choice(glob.glob(file_path + '*'))
-    path=np.random.choice(glob.glob(path + "/*."+ext),2)
+def create_pair_rgb(path,single,ext='jpg'):
     #print(path)
-    
     img1=single(path[0])
     img2=single(path[1])
     '''
@@ -61,48 +67,41 @@ def create_positive_rgb(file_path,single,ext='jpg'):
     '''
     return np.array([img1, img2])
     
-def create_negative_rgb(file_path,single,ext='jpg'):
-    path=np.random.choice(glob.glob(file_path + '*'),2)
-    path=[np.random.choice(glob.glob(path[0] + "\\*."+ext)),np.random.choice(glob.glob(path[1] + "\\*."+ext))]
-    #print(path)
-    
-    img1=single(path[0])
-    img2=single(path[1])
-    '''
-    plt.imshow(img1)
-    plt.show()
-    plt.imshow(img2)
-    plt.show()
-    '''
-    
-    return np.array([img1, img2])
-    
-def generator(path,single,batch_size=16,shape=(2,112,112,3),ext='jpg'):
-  
+def generator(path,single,batch_size=16,shape=(2,128,128,3),ext='jpg'):
+  path=glob.glob(path+'*')
   while 1:
     X=[]
     y=[]
     switch=True
     for _ in range(batch_size):
       if switch:
-        X.append(create_positive_rgb(path,single,ext=ext).reshape(shape))
+        pathp=get_dir(path,'positive',ext)
+        X.append(create_pair_rgb(pathp,single,ext=ext).reshape(shape))
         y.append(np.array([0.]))
       else:
-        X.append(create_negative_rgb(path,single,ext=ext).reshape(shape))
+        pathn=get_dir(path,'negative',ext)
+        X.append(create_pair_rgb(pathn,single,ext=ext).reshape(shape))
         y.append(np.array([1.]))
       switch=not switch
     X = np.asarray(X)
     y = np.asarray(y)
     x=[X[:,0],X[:,1]]
-    #print(len(x),len(X))
-    #print(y)
-    yield [X[:,0],X[:,1]],y
+    yield x,y
 
-gen_vap = generator(train_dir,create_single_VAPRGBD,ext='bmp')
-val_gen_vap = generator(val_dir,create_single_VAPRGBD,batch_size=4,ext='bmp')
+gen_vap = generator(train_dir,create_single_VAPRGBD,ext='bmp',shape=(2,112,112,3))
+val_gen_vap = generator(val_dir,create_single_VAPRGBD,batch_size=4,ext='bmp',shape=(2,112,112,3))
+gen_vgg = generator(train_dir_vgg,create_single_VGGFACE)
+val_gen_vgg = generator(val_dir_vgg,create_single_VGGFACE,batch_size=4)
+gen_vgg2 = generator(train_dir_vgg2,create_single_VGGFACE,batch_size=32)
+val_gen_vgg2 = generator(val_dir_vgg2,create_single_VGGFACE,batch_size=16)
 
-gen_vgg = generator(train_dir_vgg,create_single_VGGFACE,shape=(2,128,128,3))
-val_gen_vgg = generator(val_dir_vgg,create_single_VGGFACE,batch_size=4,shape=(2,128,128,3))
+#AM-Softmax/LMCL
+
+#Not Finished
+def singleGenerator(path,single=create_single_VGGFACE,ext='jpg'):
+    path=glob.glob(path+'*')
+    while 1:
+        x,y=[],[]
 
 if __name__=='__main__':
     pass
